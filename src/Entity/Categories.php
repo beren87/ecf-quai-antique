502e5aed9f3b73a289130a1535191eb7a8925792
Berenger FERGUENIS <berenger@MacBook-Pro-de-Berenger.local>
2023-02-27 21:59:59 +0100

    feat/entity: Creation of entities, migration in the database

---
+++ src/Entity/Categories.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoriesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoriesRepository::class)]
+class Categories
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\ManyToMany(targetEntity: Dishes::class, inversedBy: 'categories')]
+    private Collection $dishes;
+
+    public function __construct()
+    {
+        $this->dishes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Dishes>
+     */
+    public function getDishes(): Collection
+    {
+        return $this->dishes;
+    }
+
+    public function addDish(Dishes $dish): self
+    {
+        if (!$this->dishes->contains($dish)) {
+            $this->dishes->add($dish);
+        }
+
+        return $this;
+    }
+
+    public function removeDish(Dishes $dish): self
+    {
+        $this->dishes->removeElement($dish);
+
+        return $this;
+    }
+}
