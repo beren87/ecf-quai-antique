f7be74fd3b519a6a8a6db0d7489622d79073ca37
Berenger FERGUENIS <berenger@MacBook-Pro-de-Berenger.local>
2023-03-02 13:42:14 +0100

    feat/fixtures: Create CategoriesFixtures, no faker

---
+++ src/DataFixtures/CategoriesFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Categories;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoriesFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $category = new Categories();
+        $category->setTitle('Soupe à l’oignon');
+        $category->setDescription('Soupe à l’oignon, crouton aillés, gratiné de parmesan');
+        $category->setPrice(16);
+        $manager->persist($category);
+
+        $category = new Categories();
+        $category->setTitle('Matouille de Savoie');
+        $category->setDescription('Matouille de Savoie, fondu de poireau, carottes glacées');
+        $category->setPrice(24);
+        $manager->persist($category);
+
+        $manager->flush();
+    }
+}
