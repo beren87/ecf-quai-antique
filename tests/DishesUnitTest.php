b28df807a6a3285c76be9123a856c26411b90da2
Berenger FERGUENIS <berenger@MacBook-Pro-de-Berenger.local>
2023-03-03 11:50:03 +0100

    feat/unit_test: rectify unit tests for Dishes and Categories

--- tests/DishesUnitTest.php
+++ tests/DishesUnitTest.php
@@ -12,10 +12,14 @@ class DishesUnitTest extends TestCase
         $users = new Users();
         $dishes = new Dishes();
 
-        $dishes->setName('nom');
+        $dishes->setTitle('fondu');
+        $dishes->setDescription('fondu savoyarde du quai antique');
+        $dishes->setPrice(24.99);
         $dishes->setUsers($users);
 
-        $this->assertTrue($dishes->getName()=== 'nom');
+        $this->assertTrue($dishes->getTitle()=== 'fondu');
+        $this->assertTrue($dishes->getDescription()=== 'fondu savoyarde du quai antique');
+        $this->assertTrue($dishes->getPrice()=== 24.99);
         $this->assertTrue($dishes->getUsers()=== $users);
     }
     public function testIsFalse()
@@ -23,17 +27,23 @@ class DishesUnitTest extends TestCase
         $users = new Users();
         $dishes = new Dishes();
 
-        $dishes->setName('nom');
+        $dishes->setTitle('fondu');
+        $dishes->setDescription('fondu savoyarde du quai antique');
+        $dishes->setPrice(24);
         $dishes->setUsers($users);
 
-        $this->assertFalse($dishes->getName()=== 'false');
+        $this->assertFalse($dishes->getTitle()=== 'tartiflette');
+        $this->assertFalse($dishes->getDescription()=== 'tartiflette de la mer');
+        $this->assertFalse($dishes->getPrice()=== 26);
         $this->assertFalse($dishes->getUsers()=== new Users());
     }
     public function testIsEmpty()
     {
         $dishes = new Dishes();
 
-        $this->assertEmpty($dishes->getName());
+        $this->assertEmpty($dishes->getTitle());
+        $this->assertEmpty($dishes->getDescription());
+        $this->assertEmpty($dishes->getPrice());
         $this->assertEmpty($dishes->getUsers());
     }
 }
