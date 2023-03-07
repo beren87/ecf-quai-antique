dafd992ba2b7ddb8178c9b1ebf4b390abfb06f51
Berenger FERGUENIS <berenger@MacBook-Pro-de-Berenger.local>
2023-03-07 20:42:15 +0100

    feat/easy-admin: add Categorie and DisheCrudController

---
+++ src/Controller/Admin/CategorieCrudController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Categorie;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminbundle\Config\Crud;
+
+class CategorieCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Categorie::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name'),
+        ];
+    }
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setDefaultSort(['name'=>'DESC']);
+    }
+}
---
+++ src/Controller/Admin/DisheCrudController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Dishe;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextAreaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminbundle\Config\Crud;
+
+class DisheCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Dishe::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('title'),
+            TextAreaField::new('description'),
+            NumberField::new('price'),
+        ];
+    }
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setDefaultSort(['title'=>'DESC']);
+    }
+    
+}
