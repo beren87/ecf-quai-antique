1096db996f8082a1779a34e84402c753b06cbe0a
Berenger FERGUENIS <berenger@MacBook-Pro-de-Berenger.local>
2023-03-14 20:21:20 +0100

    feat/fixtures: add ReservationFixtures, add user in ReservationService

--- src/Controller/ReservationsController.php
+++ src/Controller/ReservationsController.php
@@ -8,7 +8,6 @@ use App\Form\ReservationsFormType;
 use App\Repository\OpeningHourRepository;
 use App\Repository\RestaurantRepository;
 use App\Service\ReservationService;
-use DateTime;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,22 +15,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class ReservationsController extends AbstractController
 {
     #[Route('/reservations', name: 'app_reservations')]
-    public function index(Request $request, RestaurantRepository $restaurantRepository, ReservationService $reservationService, OpeningHourRepository $openingHourRepository): Response
+    public function index(Request $request, 
+    RestaurantRepository $restaurantRepository, 
+    ReservationService $reservationService, 
+    OpeningHourRepository $openingHourRepository): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
         //création d'une réservation
         $reservations = new Reservations();   
+
         //création du formulaire
         $reservationForm = $this->createForm(ReservationsFormType::class, $reservations);
+
         //traite de la requete du formulaire
         $reservationForm->handleRequest($request); 
-        
+
+        //Si Formulaire soumis et valide, alors on ajoute l'objet $reservations dans la BDD
         if ($reservationForm ->isSubmitted() && $reservationForm->isValid()){
             $reservations = $reservationForm->getData();
             
             $reservationService->persistReservation($reservations);
 
             return $this->redirectToRoute('app_reservations');
-
+           
             }
             return $this->render('reservations/index.html.twig', [
                 'reservationForm' => $reservationForm->createView(),
